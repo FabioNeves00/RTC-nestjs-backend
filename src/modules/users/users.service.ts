845a6d2216eb773a1f6a51465317109a36a27bdf
Fabio Neves <fabinhoneves09@gmail.com>
2023-02-02 18:16:01 -0300

    feat(user): created user methods

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
+
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -8,19 +8,15 @@ export class UsersService {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  getUserContacts(user: string) {
+    return 'This action returns all user contacts';
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  addUserToContacts(user: string, userCodeToBeAdded: string) {
+    return 'This action adds an user to the list of contacts of another user';
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  removeUserFromContacts(user: string, userCodeToBeRemoved: string) {
+    return 'This action adds an user to the list of contacts of another user';
   }
 }
