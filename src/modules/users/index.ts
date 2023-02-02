f745761f17ea3b0da3cdc639f82bf43a092cc947
Fabio Neves <fabinhoneves09@gmail.com>
2023-02-02 18:18:02 -0300

    feat(user): created user routes

--- src/modules/users/index.ts
+++ src/modules/users/index.ts
@@ -2,6 +2,5 @@ export * from './users.controller';
 export * from './users.service';
 export * from './users.module';
 export * from './dto/create-user.dto';
-export * from './dto/update-user.dto';
 export * from './entities/user.entity';
 export * from './types';
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -13,22 +12,17 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  getUserContacts(user: string) {
+    return this.usersService.getUserContacts(user);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Put()
+  addUserToContacts(user: string, userCodeToBeAdded: string) {
+    return this.usersService.addUserToContacts(user, userCodeToBeAdded);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Delete()
+  removeUserFromContacts(user: string, userCodeToBeRemoved: string) {
+    return this.usersService.removeUserFromContacts(user, userCodeToBeRemoved);
   }
 }
