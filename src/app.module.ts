8780a248c414c9f6319f2104ef2d445dcec1d446
Fabio Neves <fabinhoneves09@gmail.com>
2023-02-02 10:42:20 -0300

    refactor(modules): changed user module name to plural

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,18 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { configurationService } from './config/config.service';
-import { UserModule } from './modules/user/user.module';
 import { MessagesModule } from './modules/messages/messages.module';
 import { RoomsModule } from './modules/rooms/rooms.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(configurationService.getTypeOrmConfig(__dirname)),
     MailerModule.forRoot(configurationService.getNodeMailerConfig()),
-    UserModule,
     MessagesModule,
     RoomsModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [],
