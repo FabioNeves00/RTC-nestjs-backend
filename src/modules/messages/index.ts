d59b0efcf0810af5f70af483cd273a93ad56f532
Fabio Neves <fabinhoneves09@gmail.com>
2023-02-02 18:23:34 -0300

    refactor(messages): removed update and remove from messages

--- src/modules/messages/index.ts
+++ src/modules/messages/index.ts
@@ -2,6 +2,5 @@ export * from './messages.gateway';
 export * from './messages.service';
 export * from './messages.module';
 export * from './dto/create-message.dto';
-export * from './dto/update-message.dto';
 export * from './entities/message.entity';
 export * from './types';
--- src/modules/messages/messages.gateway.ts
+++ src/modules/messages/messages.gateway.ts
@@ -5,7 +5,6 @@ import {
 } from '@nestjs/websockets';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
 
 @WebSocketGateway()
 export class MessagesGateway {
